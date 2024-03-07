1b981cc767de8b547745765e8c9dd38048668d59
i-am-jiwon <dlark1128@gmail.com>
2024-03-07 19:36:36 +0900

    feature : terraform 24.03.07

--- main.tf
+++ main.tf
@@ -244,10 +244,10 @@ ${local.ec2_user_data_base}
 
 docker login ghcr.io -u ${var.docker_username} -p ${var.docker_password}
 
-mkdir -p /docker_projects/gha
-curl -o /docker_projects/gha/zero_downtime_deploy.py https://raw.githubusercontent.com/E-IT-Charge/E-IT-Charge-Api-Server/feature/mainGHA/infraScript/zero_downtime_deploy.py
-chmod +x /docker_projects/gha/zero_downtime_deploy.py
-/docker_projects/gha/zero_downtime_deploy.py
+mkdir -p /docker_projects/everycharge
+curl -o /docker_projects/everycharge/zero_downtime_deploy.py https://raw.githubusercontent.com/E-IT-Charge/E-IT-Charge-Api-Server/feature/mainGHA/infraScript/zero_downtime_deploy.py
+chmod +x /docker_projects/everycharge/zero_downtime_deploy.py
+/docker_projects/everycharge/zero_downtime_deploy.py
 EOF
 }
 
